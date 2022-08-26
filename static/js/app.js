c21b6b66ddc04bcc4477f4ff1c8c0ab6e52bbc1a
Kevin Eapen <kevineapen777@gmail.com>
2022-08-26 12:21:18 -0700

    Add table data filter and user click event listener.

--- static/js/app.js
+++ static/js/app.js
@@ -27,3 +27,31 @@ data.forEach((dataRow) => {
     cell.text(val);
     });
 });
+
+// Create function to filter table by user input date
+function handleClick() {
+    // Use d3 lib to find html datetime tag and get date value from filter
+    let date = d3.select("#datetime").property("value");
+    // Set default filter (original table data)
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the data using 
+    // that date
+    if (date) {
+        // Apply 'filter' to the table data to only keep the rows where 
+        // the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will just be
+    // the original tableData
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
